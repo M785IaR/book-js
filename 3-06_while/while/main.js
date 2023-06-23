b6249f911ef00b0cd1627cbba9ddd30ab10cf45e
M785IaR <131901147+M785IaR@users.noreply.github.com>
2023-06-23 16:12:11 +0900

    mark up

---
+++ 3-06_while/while/main.js
@@ -0,0 +1,14 @@
+//コンソールに１~１０の数字を連続で出力する
+console.log('for')
+
+for(let i = 0; i < 11; i ++ ){
+    console.log(i);
+}
+//i++=最後に1を足す（i + 1でもok）、++i=最初に1足す
+console.log('while')
+
+let i = 0;
+while(i < 11){
+    console.log(i);
+    i ++;
+}
